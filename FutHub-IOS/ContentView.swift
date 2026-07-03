85497dd9e77755a669ece2bcf9761016a2bdbe2e
Jeancarlo <jeancarlo.ricardo@triosstudent.com>
2026-07-03 13:09:48 -0400

    Initial Commit

---
+++ FutHub-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FutHub-IOS
+//
+//  Created by Jeancarlo on 2026-07-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FutHub-IOSTests/FutHub_IOSTests.swift
@@ -0,0 +1,19 @@
+//
+//  FutHub_IOSTests.swift
+//  FutHub-IOSTests
+//
+//  Created by Jeancarlo on 2026-07-03.
+//
+
+import Testing
+@testable import FutHub_IOS
+
+struct FutHub_IOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
